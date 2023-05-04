b678ecb780b92308d181f20952c01f630ef7f400
vrishali183 <vrishali183@gmail.com>
2023-05-04 06:16:53 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 function addUser(){
     var user_name=document.getElementById("user_name").value;
     localStorage.setItem("user_name",user_name); 
-    window.location="chat_room.html";
+    //add chat_room.html
+    window.location="";
     
     }
